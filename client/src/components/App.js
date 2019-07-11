25e5fcdbd163b1614ee71815ba6b098b9a89ffd6
sahilthakur7 <sahilrd07@gmail.com>
2019-07-11 22:44:57 +0530

    added materialize

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
 const App = () => {
     return(
         <div>
-            Hi there!
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>
         </div>
     );
 };
