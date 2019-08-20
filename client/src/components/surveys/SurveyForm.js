ea3e850dd7f30788ebf6ac203920f8d5f147689c
sahilthakur7 <sahilrd07@gmail.com>
2019-08-20 23:17:44 +0530

    survey form ready

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { reduxForm , Field } from 'redux-form';
+import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
+
+const FIELDS = [
+    {label: 'Survey Title', name: 'title'},
+    {label: 'Subject', name: 'subject'},
+    {label: 'Email Body', name: 'body'},
+    {label: 'Recepient List', name: 'emails'}
+
+];
 
 class SurveyForm extends React.Component {
+    renderFields = () => {
+
+        const formFields = FIELDS.map((field) => {
+            return(
+            <Field key={`${field.name}`} name={`${field.name}`} label={`${field.label}`} component={SurveyField} type="text" />
+            );
+        });
+        return formFields;
+    }
+
     render(){
         return(
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-                    <Field type="text" name="surveyTitle" component="input" />
-                    <button type="submit">Submit</button>
+                    {this.renderFields()}
+                    <Link to={`/surveys`} className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         )
